69dc13d629a590cc75bb88f1f54618431bc7c4d0
melloware <mellowaredev@gmail.com>
2020-07-25 09:00:51 -0400

    Fix Travis so Chrome 83 matches installed Chrome version

--- .travis/install-chromedriver.sh
+++ .travis/install-chromedriver.sh
@@ -3,7 +3,7 @@
 set -euf
 
 LATEST_CHROME_VERSION="`google-chrome-stable --version | sed -E 's/(^Google Chrome |\.[0-9]+ )//g'`"
-LATEST_CHROMEDRIVER_VERSION="`curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE`"
+LATEST_CHROMEDRIVER_VERSION="`curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE_83`"
 CHROMEDRIVER_FILENAME="chromedriver_linux64.zip"
 
 printf "LATEST_CHROME_VERSION=%s\nLATEST_CHROMEDRIVER_VERSION=%s\n" "${LATEST_CHROME_VERSION}" "${LATEST_CHROMEDRIVER_VERSION}"
