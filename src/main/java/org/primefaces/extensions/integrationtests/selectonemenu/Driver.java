6b286fe4d45bdbbec40c946205f2b65d2e8489ff
melloware <mellowaredev@gmail.com>
2020-08-23 09:19:34 -0400

    Made serializable

--- src/main/java/org/primefaces/extensions/integrationtests/selectonemenu/Driver.java
+++ src/main/java/org/primefaces/extensions/integrationtests/selectonemenu/Driver.java
@@ -1,11 +1,14 @@
 package org.primefaces.extensions.integrationtests.selectonemenu;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class Driver {
+public class Driver implements Serializable {
+    private static final long serialVersionUID = 8371414176714192877L;
     private int id;
     private String name;
 }
