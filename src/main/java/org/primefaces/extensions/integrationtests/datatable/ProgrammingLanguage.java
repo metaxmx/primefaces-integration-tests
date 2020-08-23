125a428acbf52bd2fbc2b26bb4ea811d4679a987
melloware <mellowaredev@gmail.com>
2020-08-23 09:14:51 -0400

    Made serializable

--- src/main/java/org/primefaces/extensions/integrationtests/datatable/ProgrammingLanguage.java
+++ src/main/java/org/primefaces/extensions/integrationtests/datatable/ProgrammingLanguage.java
@@ -1,11 +1,14 @@
 package org.primefaces.extensions.integrationtests.datatable;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class ProgrammingLanguage {
+public class ProgrammingLanguage implements Serializable {
+    private static final long serialVersionUID = 398626647627541586L;
     private int id;
     private String name;
     private int firstAppeared;
