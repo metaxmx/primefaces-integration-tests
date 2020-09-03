1018a5f2cfba0b7bd2482e9db4dd1e5db6641e93
melloware <mellowaredev@gmail.com>
2020-09-03 07:08:40 -0400

    Dialog made serializable

--- src/main/java/org/primefaces/extensions/integrationtests/dialog/DialogData.java
+++ src/main/java/org/primefaces/extensions/integrationtests/dialog/DialogData.java
@@ -1,8 +1,11 @@
 package org.primefaces.extensions.integrationtests.dialog;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class DialogData {
+public class DialogData implements Serializable {
+    private static final long serialVersionUID = 1934462033877094891L;
     private String text;
 }
