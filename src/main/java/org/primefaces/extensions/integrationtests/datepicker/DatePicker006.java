18e6a35b5220e9fd0ee2d913f2f969c69f2a37da
melloware <mellowaredev@gmail.com>
2020-11-13 13:08:21 -0500

    Test #6506 DatePicker min/max using java.sql.Date

---
+++ src/main/java/org/primefaces/extensions/integrationtests/datepicker/DatePicker006.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2011-2020 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.integrationtests.datepicker;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import lombok.Data;
+
+@Named
+@ViewScoped
+@Data
+public class DatePicker006 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private LocalDate localDate;
+    private java.sql.Date minDate;
+    private java.sql.Date maxDate;
+
+    @PostConstruct
+    public void init() {
+        localDate = LocalDate.now();
+        minDate = java.sql.Date.valueOf(localDate.minusMonths(1));
+        maxDate = java.sql.Date.valueOf(localDate.plusMonths(1));
+        localDate = null;
+    }
+
+    public void submit() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "SQL Date", localDate.toString()));
+    }
+
+}
---
+++ src/test/java/org/primefaces/extensions/integrationtests/datepicker/DatePicker006Test.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2011-2020 PrimeFaces Extensions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.extensions.integrationtests.datepicker;
+
+import java.time.LocalDate;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.FindBy;
+import org.primefaces.extensions.selenium.AbstractPrimePage;
+import org.primefaces.extensions.selenium.AbstractPrimePageTest;
+import org.primefaces.extensions.selenium.component.CommandButton;
+import org.primefaces.extensions.selenium.component.DatePicker;
+import org.primefaces.extensions.selenium.component.Messages;
+
+public class DatePicker006Test extends AbstractPrimePageTest {
+
+    @Test
+    @Order(1)
+    @DisplayName("DatePicker: GitHub #6506 using java.sql.Date")
+    public void testPreselectedRange(Page page) {
+        // Arrange
+        DatePicker datePicker = page.datePicker;
+        Assertions.assertEquals(null, datePicker.getValue());
+        LocalDate value = LocalDate.now();
+
+        // Act
+        datePicker.setValue(value);
+        page.button.click();
+
+        // Assert
+        Assertions.assertEquals(value.toString(), page.messages.getMessage(0).getDetail());
+        assertConfiguration(datePicker.getWidgetConfiguration());
+    }
+
+    private void assertConfiguration(JSONObject cfg) {
+        assertNoJavascriptErrors();
+        System.out.println("DatePicker Config = " + cfg);
+        Assertions.assertEquals("mm/dd/yy", cfg.getString("dateFormat"));
+        Assertions.assertEquals("single", cfg.getString("selectionMode"));
+        Assertions.assertFalse(cfg.getBoolean("inline"));
+    }
+
+    public static class Page extends AbstractPrimePage {
+        @FindBy(id = "form:msgs")
+        Messages messages;
+
+        @FindBy(id = "form:datepicker")
+        DatePicker datePicker;
+
+        @FindBy(id = "form:button")
+        CommandButton button;
+
+        @Override
+        public String getLocation() {
+            return "datepicker/datePicker006.xhtml";
+        }
+    }
+}
